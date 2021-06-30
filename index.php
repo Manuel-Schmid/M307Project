001a97098b5e0bba9a4672d0fefe1cef9a04a268
Manuel Schmid <manysch3@gmail.com>
2021-06-30 10:17:45 +0200

    Implemented MVC-Pattern

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="View/Styles.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="Views/Styles.css">
+    <script src="Controllers/script.js"></script>
     <title>M307 Projekt</title>
 </head>
 <body>
 
 <?php
-require("database.php");
+require("Models/database.php");
 $statement = $pdo->prepare('SELECT * FROM user');
 $statement->execute();
 $result = $statement->fetchAll();
