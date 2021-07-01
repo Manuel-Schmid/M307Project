df2a4b745e5f5bca7baab2ad6e25490083d1e0c9
Manuel Schmid <manysch3@gmail.com>
2021-07-01 03:57:49 +0200

    startet implementing updateMortgageView

--- Controllers/updateMortageScript.js
+++ Controllers/updateMortageScript.js
@@ -3,16 +3,29 @@ console.log('script loaded')
 let editing = false;
 
 function editField(id, placeholder) {
-    console.log(placeholder)
     if (!editing) {
-        document.getElementById(id).innerHTML = '<input id="'+id+'"type="text" class="input-field" placeholder="'+placeholder+'">';
+        document.getElementById(id).innerHTML = '<input name="' + placeholder + '" id="' + id + '"type="text" class="input-field" placeholder="' + placeholder + '">';
         document.getElementById(id).style.paddingLeft = 0 + 'px';
-        document.getElementById(id + '-btn').innerText = "Abbrechen";
+        // document.getElementById(id + '-btn').innerText = "Abbrechen";
+        document.getElementById(id + '-btn').outerHTML = '<button id="' + id + "-save" + '" class="small-button" onclick="saveChanges('+id+')"><p>&#x2705</p></button> <button id="' + id + "-cncl" + '" class="small-button" onclick="saveChanges(false)"><p>&#x274E</p></button>'
         editing = true;
-    } else {
-        document.getElementById(id).innerHTML = '<td id='+id+'>'+placeholder+'</td>';
-        document.getElementById(id).style = null;
-        document.getElementById(id + '-btn').innerText = "Bearbeiten";
-        editing = false;
     }
+    // } else {
+    //     document.getElementById(id).innerHTML = '<td id='+id+'>'+placeholder+'</td>';
+    //     document.getElementById(id).style = null;
+    //     document.getElementById(id + '-btn').innerText = "Bearbeiten";
+    //     editing = false;
+    // }
 }
+
+function saveChanges(id) {
+    console.log("id: ")
+    console.log(id)
+    // let vals = document.getElementById(id).value;
+    // console.log(vals)
+    // if(saveChanges){
+        // console.log(input)
+    // }
+}
+
+// onclick="editField('firstName', '<?=$var?>')
