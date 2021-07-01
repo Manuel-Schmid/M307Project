f232e07aa8cee51c4bfc418889481e52b956b683
Lewin Rutz <lewin.rutz@gmail.com>
2021-07-02 00:10:53 +0200

    Destroyed all the inputs in the updateMortgageView.php, because of the validation.js

--- Controllers/validation.js
+++ Controllers/validation.js
@@ -3,13 +3,12 @@ errors = []
 function checkEmpty(value, field) {
     if (value === ""){
         errors.push(field + " muss aufgefüllt sein.")
-
     }
 }
 
 function checkAt(value) {
     if(value.contains("#")===false){
-        errors.push("Die E-Mail ist ungültig")
+        errors.push("Die E-Mail ist ungültig");
     }
 }
 
@@ -17,4 +16,29 @@ function checkNumbers(value){
     if(!value.match(/[\+0-9]/g)){
     errors.push("Diese Telefonnummer ist ungültig");
     }
+}
+
+function getErrors(){
+    return errors;
+}
+
+function checkErrors(){
+    if(errors.length===0){
+        return true;
+    }
+    return false;
+}
+
+function createList(){
+
+    var listView=document.createElement('ol');
+
+    for(var i=0;i<errors.length;i++)
+    {
+        var listViewItem=document.createElement('li');
+        listViewItem.appendChild(document.createTextNode(errors[i]));
+        listView.appendChild(listViewItem);
+    }
+
+    return listView;
 }
